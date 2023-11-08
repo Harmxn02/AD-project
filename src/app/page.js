78b79f47bf51de951cbced790aae734308449af9
Harman Singh <harman.singh@student.howest.be>
2023-11-08 15:12:05 +0100

    Create component and import it to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
 import Image from 'next/image'
 import Homescreen from '@/components/homescreen/Homescreen'
 import Account from '@/components/settings/Account'
+import AccountDeleted from '@/components/settings/AccountDeleted'
 
 export default function Home() {
   return (
     <>
       {/* <Homescreen/> */}
       {/* <Account/> */}
+
+
+
+
+      
+      <AccountDeleted/>
     </>
     
   )
