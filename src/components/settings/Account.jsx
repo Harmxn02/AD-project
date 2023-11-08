cfe407be261fbf304001cafe1001d299f3cf6534
Harman Singh <harman.singh@student.howest.be>
2023-11-08 11:42:24 +0100

    create DANGER ZONE section of Account.jsx

--- src/components/settings/Account.jsx
+++ src/components/settings/Account.jsx
@@ -5,11 +5,11 @@ import Title from '../utility/Title'
 const Account = () => {
   return (
     <>
-        <section>
-            <Title content="Account"/>
+        <Title content="Account"/>
+        <section className='flex flex-col gap-5'>
 
             {/* Active Subscription */}
-            <div className='w-4/6 border border-[#B3B3B3] py-6 px-4 flex gap-5 '>
+            <div className='bg-white w-4/6 border border-[#B3B3B3] py-6 px-4 flex gap-5 '>
 
                 <div>
                     {/* ICON */}
@@ -27,13 +27,31 @@ const Account = () => {
                         <a href="#todo" className='border border-black px-4 py-3 font-medium'>Cancel subscription</a>
                     </div>
                 </div>
-
-
-
-
             </div>
 
             {/* Danger Zone */}
+            <div className='bg-white w-4/6 border border-[#B3B3B3] py-6 px-4 flex gap-5 '>
+
+                <div>
+                    {/* ICON */}
+                    <div className='w-8 h-8 bg-black'></div>
+                </div>
+                <div className='flex flex-col gap-4'>
+                    {/* CONTENT */}
+                    <div>
+                        <h3 className='font-medium text-2xl'>DANGER ZONE</h3>
+                        <p className='font-light'>Delete your account. This is a 
+                            <span className='uppercase font-bold'> PERMANENT</span> and 
+                            <span className='uppercase font-bold'> IRREVERSIBLE</span>  change.</p>
+                    </div>
+
+                    <div className='flex gap-5'>
+                        <a href="#todo" className='border bg-black px-6 py-3 font-black text-white'>DELETE ACCOUNT</a>
+                    </div>
+                </div>
+            </div>
+
+    
 
 
 
