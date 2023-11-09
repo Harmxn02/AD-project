52faa2696fa1113d1810cbe5799b3e470c7326a4
Harman Singh <harman.singh@student.howest.be>
2023-11-09 15:38:31 +0100

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+ 
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        outDir: "src/assets/js/dist",
+        rollupOptions: {
+            input: "src/assets/js/App.jsx"
+        },
+    }
+    // Other configuration options as needed
+});
