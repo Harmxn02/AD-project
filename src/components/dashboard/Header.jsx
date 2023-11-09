3a92582e6aead4cd6898a02f7231516c5688ab62
layton <layton.berth@student.howest.be>
2023-11-09 11:59:27 +0100

    add create screenshot button

--- src/components/dashboard/Header.jsx
+++ src/components/dashboard/Header.jsx
@@ -29,18 +29,19 @@ const Header = () => {
     };
 
   return (
-    <div>
+    <div className='flex justify-between'>
         <div className='flex gap-12'>
             {buttons.map((button, index) => (
             <button
                 key={index}
                 onClick={() => handleButtonClick(button.title)}
-                className={`font-medium py-1 px-4 ${activeButton === button.title ? 'bg-[#747474] text-white' : ''}`}
+                className={`font-medium py-2 px-4 ${activeButton === button.title ? 'bg-[#747474] text-white' : ''}`}
             >
                 {button.title}
             </button>
             ))}
         </div>
+        <button className='bg-[#747474] text-white px-4 py-2'>CREATE SCREENSHOT</button>
     </div>
   )
 }
