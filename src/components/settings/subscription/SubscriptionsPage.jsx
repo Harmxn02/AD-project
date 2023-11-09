f41999b5f9447895fbd783443ae1f2120eb40eb4
Harman Singh <harman.singh@student.howest.be>
2023-11-09 12:52:12 +0100

    create object with 3-plan data

--- src/components/settings/subscription/SubscriptionsPage.jsx
+++ src/components/settings/subscription/SubscriptionsPage.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 const SubscriptionsPage = () => {
+	const subscriptionData = [
+		{ price: "21.99" ,  name: "Lewis & Clark", perks: ["10 hours flight time", "Specialized training", "Customer support", "Max 1 rental/week"] },
+		{ price: "33.99" ,  name: "Marco Polo", perks: ["Everything included in L&C...", "24 hours flight time", "Priority during peak hours"] },
+		{ price: "39.99" ,  name: "Magellan", perks: ["Everything included in MP...", "48 hours flight time", "24/7 customer support", "Unlimited rental opportunities"] },
+	];
 
 	return (
 	);
