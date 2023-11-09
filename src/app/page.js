e5cf1b704660b84b5ca4b526647c92df0a94565f
Harman Singh <harman.singh@student.howest.be>
2023-11-09 12:55:09 +0100

    import SubscriptionsPage to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,20 @@
-import Image from 'next/image'
-import Homescreen from '@/components/homescreen/Homescreen'
-import Account from '@/components/settings/Account'
-import AccountDeleted from '@/components/settings/AccountDeleted'
-import Transactions from '@/components/transactions/Transactions'
-import DeleteAccountPrompt from '@/components/settings/DeleteAccountPrompt'
+import Image from "next/image";
+import Homescreen from "@/components/homescreen/Homescreen";
+import Account from "@/components/settings/Account";
+import AccountDeleted from "@/components/settings/AccountDeleted";
+import Transactions from "@/components/transactions/Transactions";
+import DeleteAccountPrompt from "@/components/settings/DeleteAccountPrompt";
+import SubscriptionsPage from "@/components/settings/subscription/SubscriptionsPage";
 
 export default function Home() {
-  return (
-    <>
-      {/* <Homescreen/> */}
-      {/* <Account/> */}
-      {/* <AccountDeleted/> */}
-      {/* <Transactions/> */}
-      {/* <DeleteAccountPrompt/> */}
-    </>
-    
-  )
+	return (
+		<>
+			{/* <Homescreen/> */}
+			{/* <Account/> */}
+			{/* <AccountDeleted/> */}
+			{/* <Transactions/> */}
+			{/* <DeleteAccountPrompt/> */}
+			{/* <SubscriptionsPage /> */}
+		</>
+	);
 }
