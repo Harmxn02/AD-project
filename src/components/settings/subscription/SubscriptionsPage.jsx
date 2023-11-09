5b734ca073db2b145267a50eca77f31953800ed7
Harman Singh <harman.singh@student.howest.be>
2023-11-09 12:54:53 +0100

    work out SubscriptionCard, and style last element differently

--- src/components/settings/subscription/SubscriptionsPage.jsx
+++ src/components/settings/subscription/SubscriptionsPage.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Title from "../../utility/Title";
+import SubscriptionCard from "./SubscriptionCard";
+import ListItem from "./ListItem";
+import VerticalSubscriptionCard from "./VerticalSubscriptionCard";
+
 const SubscriptionsPage = () => {
 	const subscriptionData = [
 		{ price: "21.99" ,  name: "Lewis & Clark", perks: ["10 hours flight time", "Specialized training", "Customer support", "Max 1 rental/week"] },
@@ -9,6 +14,16 @@ const SubscriptionsPage = () => {
 	return (
 		<div>
 			<Title content="Subscription Plans" />
+			<div className="grid grid-cols-3">
+				{subscriptionData.map((subscription, index) => (
+					<SubscriptionCard isLast={index === subscriptionData.length - 1}  key={index} price={subscription.price} name={subscription.name}>
+						{subscription.perks.map((perk, perkIndex) => (
+							<ListItem key={perkIndex} content={perk} />
+						))}
+					</SubscriptionCard>
+				))}
+				<VerticalSubscriptionCard/>
+			</div>
 		</div>
 	);
 };
