e583fb86ea44a15b2f699ab0ed6317efa24186d2
layton <layton.berth@student.howest.be>
2023-11-10 10:59:44 +0100

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import './App.css'
-// import Header from './components/dashboard/Header'
-// import Sidebar from './components/dashboard/Sidebar'
-// import Homescreen from './components/homescreen/Homescreen'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Transactions from './pages/Transactions';
+import Settings from './pages/Settings';
+import AccountDeleted from './pages/AccountDeleted';
+import DeleteAccount from './pages/DeleteAccount';
 
 function App() {
-
   return (
-    <>
-      {/* <Header/>
-      <Sidebar/>
-      <Homescreen/> */}
-
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/settings/delete-acount" element={<DeleteAccount />} />
+          <Route path="/settings/account-deleted" element={<AccountDeleted />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
