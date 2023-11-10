cd61dfdfbc90b05e6e6c83d241f42cc982d03a8e
Harman Singh <harman.singh@student.howest.be>
2023-11-10 20:52:50 +0100

    move API function to actual file. This is way cleaner

--- src/components/settings/subscription/SubscriptionsPage.jsx
+++ src/components/settings/subscription/SubscriptionsPage.jsx
@@ -1,50 +1,26 @@
-import React, { useEffect } from "react";
+"use client"
+
+import React from "react";
 import Title from "../../utility/Title";
 import SubscriptionCard from "./SubscriptionCard";
 import ListItem from "./ListItem";
 import VerticalSubscriptionCard from "./VerticalSubscriptionCard";
 
-import fetch from 'isomorphic-fetch'
-import { useState } from 'react';
+import { GetAPI } from "@/components/utility/api";
 
 const SubscriptionsPage = ()=> {
 
+	// normally `const endpoint`, but I need to splice the first 3 elements
+	let plans = GetAPI("/plans");
 
-	const [apiData, setApiData] = useState(null);
+	if (plans === null) {
+		// this prevents map from not working if the data is empty
+		console.log("When this component was rendered, plans == ", plans)
+		return;
+	}
 
-	const URL = `https://project-2.ti.howest.be/2023-2024/group-17/api`
-  
-	const fetchData = async () => {
-	  try {
-		const response = await fetch(URL + '/plans');
-		const data = await response.json();
-		const firstKey = Object.keys(data)[0];
-		setApiData(data[firstKey].slice(0, 3));
-	  } catch (error) {
-		console.error("Error", error);
-	  } 
-	};
-  
-	// async function waitBefore() {
-	// 	await fetchData();
-	// 	console.log(apiData)
-	// }
-
-	useEffect(()  => {
-		fetchData();
-		console.log("test")
-	}, []);
-
-
-
-
-
-
-	const subscriptionData = [
-		{ price: "21.99" ,  name: "Lewis & Clark", perks: ["10 hours flight time", "Specialized training", "Customer support", "Max 1 rental/week"] },
-		{ price: "33.99" ,  name: "Marco Polo", perks: ["Everything included in L&C...", "24 hours flight time", "Priority during peak hours"] },
-		{ price: "39.99" ,  name: "Magellan", perks: ["Everything included in MP...", "48 hours flight time", "24/7 customer support", "Unlimited rental opportunities"] },
-	];
+	plans = plans.slice(0, 3)
+	console.log("PL", plans);
 
 	return (
 		<div>
@@ -52,10 +28,10 @@ const SubscriptionsPage = ()=> {
 
 			<Title content="Subscription Plans" />
 			<div className="flex justify-between">
-				{apiData ? ( apiData.map((subscription, index) => (
-					<SubscriptionCard isLast={index === subscriptionData.length - 1}  key={index} price={subscription.price} name={subscription.name}>
-						{subscription.perks.map((perk, perkIndex) => (
-							<ListItem key={perkIndex} content={perk} />
+				{plans ? ( plans.map((subscription) => (
+					<SubscriptionCard isLast={subscription.id === plans.length - 1}  key={subscription.id} price={subscription.price} name={subscription.name}>
+						{subscription.perks.map((perk) => (
+							<ListItem key={perk.id} content={perk} />
 						))}
 					</SubscriptionCard>
 				))
