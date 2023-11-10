21db765b1a01178d7555ed4c16234a0926723bfc
Harman Singh <harman.singh@student.howest.be>
2023-11-10 09:53:58 +0100

    Fix error in Homescreen.jsx

--- src/components/homescreen/Homescreen.jsx
+++ src/components/homescreen/Homescreen.jsx
@@ -1,14 +1,12 @@
-import Image from "next/image";
-import LOGO from "../../app/icon.svg"
-import Link from "next/link";
+import LOGO from "../../assets/icons/logo.svg"
 
 function Homescreen() {
 	return (
 		<div className="h-screen bg-[#2D2D2D] flex flex-col justify-center items-center gap-24">
-            <Image src={LOGO}/>
-			<Link href="/dashboard" className="text-white max-w-fit font-semibold uppercase bg-[#232323] px-6 py-4">
+            <img alt="logo xplor" src={LOGO}/>
+			<a href="/dashboard" className="text-white max-w-fit font-semibold uppercase bg-[#232323] px-6 py-4">
 				Enter Dashboard
-			</Link>
+			</a>
 		</div>
 	);
 }
