bcc9b557e3934dd1ac44935c59c9c99f68cce3d4
layton <layton.berth@student.howest.be>
2023-11-10 11:24:59 +0100

    add transactions component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
-import Transactions from './pages/Transactions';
+import TransactionsList from './pages/Transactions';
 import Settings from './pages/Settings';
 import AccountDeleted from './pages/AccountDeleted';
 import DeleteAccount from './pages/DeleteAccount';
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/transactions" element={<TransactionsList />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="/settings/delete-acount" element={<DeleteAccount />} />
           <Route path="/settings/account-deleted" element={<AccountDeleted />} />
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Sidebar from '../components/dashboard/Sidebar'
+import Account from '../components/settings/Account'
 
 const Settings = () => {
   return (
     <div className='flex'>
       <Sidebar />
+      <section className='pt-16 px-24 w-full'>
+        <Account/>
+      </section>
     </div>
   )
 }
