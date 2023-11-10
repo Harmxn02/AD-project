8c2c894f28db5a064ebfc1df903cb31ea216ba53
Harman Singh <harman.singh@student.howest.be>
2023-11-10 20:54:19 +0100

    Render SubscriptionsPage so we can see something when we render the client

--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -1,12 +1,17 @@
 import Account from '@/components/settings/Account'
 import Sidebar from '@/components/dashboard/Sidebar'
+import SubscriptionsPage from "../../components/settings/subscription/SubscriptionsPage"
 
 export default function Home() {
   return (
     <div className='flex'>
       <Sidebar />
       <section className='pt-16 px-24 w-full'>
-        <Account/>
+        <Account/>    
+
+        {/* should be under settings > change subscription, routing not yet implemented */}
+        {/* Im putting it here, so we can see something when we render the client */}
+        <SubscriptionsPage />
       </section>
     </div>
   )
