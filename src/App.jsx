7aa288aa30f3c97f1c7272b567a5e4954525cb03
Harman Singh <harman.singh@student.howest.be>
2023-11-10 11:42:01 +0100

    implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // import Header from './components/dashboard/Header'
 // import Sidebar from './components/dashboard/Sidebar'
 // import Homescreen from './components/homescreen/Homescreen'
+import fetchData from '../api';
+console.log(await fetchData("/plans"));
 
 function App() {
 
