87590cbc67095f602fa15778333db92483283039
Harman Singh <harman.singh@student.howest.be>
2023-11-10 16:27:22 +0100

    idk man

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,41 +1,21 @@
 "use client"
-import fetch from 'isomorphic-fetch'
+// import fetch from 'isomorphic-fetch'
 
 import Header from '@/components/dashboard/Header'
 import Sidebar from '@/components/dashboard/Sidebar'
-import { useState } from 'react';
-
+// import SubscriptionsPage from "@/components/settings/subscription/SubscriptionsPage"
 
 
 export default function Home() {
-
-  const [apiData, setApiData] = useState(null);
-
-  const URL = `https://project-2.ti.howest.be/2023-2024/group-17/api`
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch(URL + '/plans');
-      const data = await response.json();
-      setApiData(data);
-    } catch (error) {
-      console.error("Error", error);
-    } 
-  };
-
-  fetchData();
 
   return (
     <div className='flex'>
       <Sidebar />
       <section className='pt-16 px-24 w-full'>
         <Header />
-        {apiData ? (
-          <p>{JSON.stringify(apiData, null, 2)}</p>
-        ): (
-          <p>Loading ...</p>
-        )}
       </section>
+
+      {/* <SubscriptionsPage/> */}
     </div>
   )
 }
--- src/components/settings/subscription/SubscriptionsPage.jsx
+++ src/components/settings/subscription/SubscriptionsPage.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Title from "../../utility/Title";
 import SubscriptionCard from "./SubscriptionCard";
 import ListItem from "./ListItem";
 import VerticalSubscriptionCard from "./VerticalSubscriptionCard";
 
-const SubscriptionsPage = () => {
+import fetch from 'isomorphic-fetch'
+import { useState } from 'react';
+
+const SubscriptionsPage = ()=> {
+
+
+	const [apiData, setApiData] = useState(null);
+
+	const URL = `https://project-2.ti.howest.be/2023-2024/group-17/api`
+  
+	const fetchData = async () => {
+	  try {
+		const response = await fetch(URL + '/plans');
+		const data = await response.json();
+		const firstKey = Object.keys(data)[0];
+		setApiData(data[firstKey].slice(0, 3));
+	  } catch (error) {
+		console.error("Error", error);
+	  } 
+	};
+  
+	// async function waitBefore() {
+	// 	await fetchData();
+	// 	console.log(apiData)
+	// }
+
+	useEffect(()  => {
+		fetchData();
+		console.log("test")
+	}, []);
+
+	// waitBefore();
+
+
+
+
 	const subscriptionData = [
 		{ price: "21.99" ,  name: "Lewis & Clark", perks: ["10 hours flight time", "Specialized training", "Customer support", "Max 1 rental/week"] },
 		{ price: "33.99" ,  name: "Marco Polo", perks: ["Everything included in L&C...", "24 hours flight time", "Priority during peak hours"] },
@@ -13,15 +48,25 @@ const SubscriptionsPage = () => {
 
 	return (
 		<div>
+
+{/* {		apiData ? (
+          <p>{JSON.stringify(apiData, null, 2)}</p>
+        ): (
+          <p>Loading ...</p>
+        )}	 */}
+
 			<Title content="Subscription Plans" />
 			<div className="flex justify-between">
-				{subscriptionData.map((subscription, index) => (
+				{apiData ? ( apiData.map((subscription, index) => (
 					<SubscriptionCard isLast={index === subscriptionData.length - 1}  key={index} price={subscription.price} name={subscription.name}>
 						{subscription.perks.map((perk, perkIndex) => (
 							<ListItem key={perkIndex} content={perk} />
 						))}
 					</SubscriptionCard>
-				))}
+				))
+				) : (
+					<p>Loading ...</p>
+				)}
 			</div>
 				<VerticalSubscriptionCard/>
 		</div>
