8fb21b831b0c686e57e4ce85d3ffeb9d213d576c
layton <layton.berth@student.howest.be>
2023-11-10 16:36:36 +0100

    fix merge conflict

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,12 +3,7 @@
 
 import Header from '@/components/dashboard/Header'
 import Sidebar from '@/components/dashboard/Sidebar'
-<<<<<<< HEAD
 import Statistics from '../../components/dashboard/Statistics'
-=======
-// import SubscriptionsPage from "@/components/settings/subscription/SubscriptionsPage"
-
->>>>>>> 87590cbc67095f602fa15778333db92483283039
 
 export default function Home() {
 
@@ -17,13 +12,8 @@ export default function Home() {
       <Sidebar />
       <section className='pt-16 px-24 w-full'>
         <Header />
-<<<<<<< HEAD
         <Statistics />
-=======
->>>>>>> 87590cbc67095f602fa15778333db92483283039
       </section>
-
-      {/* <SubscriptionsPage/> */}
     </div>
   )
 }
