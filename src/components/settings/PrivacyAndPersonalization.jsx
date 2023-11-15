0ed9c9d89d1899f3fa7fb9f6f4d5f640821136b2
Harman Singh <harman.singh@student.howest.be>
2023-11-15 09:58:41 +0100

    add Title and Unsubscribe buttons

--- src/components/settings/PrivacyAndPersonalization.jsx
+++ src/components/settings/PrivacyAndPersonalization.jsx
@@ -2,6 +2,16 @@
 const PrivacyAndPersonalization = () => {
 	return (
 		<div>
+			<Title content="Privacy and Personalization" />
+			<section>
+				<div>
+					<h3 className="text-xl font-medium">Notification Preferences</h3>
+					<div className="flex gap-5 my-4">
+						<a href="#" className="border border-black px-4 py-3 font-medium">Unsubscribe from all Email</a>
+						<a href="#" className="border border-black px-4 py-3 font-medium">Unsubscribe from all SMS</a>
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 };
