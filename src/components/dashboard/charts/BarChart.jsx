c41881eb209b1712e16bbbdbba857e78d9f00ff2
layton <layton.berth@student.howest.be>
2023-11-15 13:57:45 +0100

    adjust component with props

--- src/components/dashboard/charts/BarChart.jsx
+++ src/components/dashboard/charts/BarChart.jsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
 
-const BarChart = () => {
+const BarChart = (props) => {
   const chartRef = useRef(null);
 
   useEffect(() => {
@@ -11,16 +11,8 @@ const BarChart = () => {
     let myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-        datasets: [
-          {
-            data: [10, 20, 30, 15, 25, 35, 45],
-            label: 'Minutes Flown',
-            backgroundColor: '#62A0AA73',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1,
-          },
-        ],
+        labels: props.labels,
+        datasets: props.dataObject,
       },
       options: {
         maintainAspectRatio: false,
