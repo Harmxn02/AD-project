7c994c948bcfa44183648e70e20e5c829a62b118
Harman Singh <harman.singh@student.howest.be>
2023-11-15 09:48:14 +0100

    import <PrivacyAndPersonalization/> component

--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -1,6 +1,7 @@
 import Account from '@/components/settings/Account'
 import Sidebar from '@/components/dashboard/Sidebar'
 import SubscriptionsPage from "../../components/settings/subscription/SubscriptionsPage"
+import PrivacyAndPersonalization from '../../components/settings/PrivacyAndPersonalization'
 
 export default function Home() {
   return (
@@ -8,10 +9,11 @@ export default function Home() {
       <Sidebar />
       <section className='pt-16 px-24 w-full'>
         <Account/>    
+        <PrivacyAndPersonalization/>
 
         {/* should be under settings > change subscription, routing not yet implemented */}
         {/* Im putting it here, so we can see something when we render the client */}
-        <SubscriptionsPage />
+        {/* <SubscriptionsPage /> */}
       </section>
     </div>
   )
