6765c17c6b46336cacfd5bf4ddd0f64c418deb2f
Harman Singh <harman.singh@student.howest.be>
2023-11-15 10:00:00 +0100

    add first section content/ excl Toggle

--- src/components/settings/PrivacyAndPersonalization.jsx
+++ src/components/settings/PrivacyAndPersonalization.jsx
@@ -11,6 +11,24 @@ const PrivacyAndPersonalization = () => {
 						<a href="#" className="border border-black px-4 py-3 font-medium">Unsubscribe from all SMS</a>
 					</div>
 				</div>
+
+				<div className="bg-white w-4/6 border border-[#B3B3B3] py-6 px-4 flex gap-5">
+					<div>
+						{/* ICON */}
+						<div className="w-8 h-8 bg-black"></div>
+					</div>
+					<div className="flex flex-col gap-4">
+						{/* CONTENT */}
+						<div>
+							<h3 className="font-medium text-2xl">
+								Payment Reminders
+							</h3>
+							<p className="font-light">
+								Send me messages for payment reminders.
+							</p>
+						</div>
+					</div>
+				</div>
 			</section>
 		</div>
 	);
