24e935b160b6ebade1baee84b52d2ac13de93ed1
Harman Singh <harman.singh@student.howest.be>
2023-11-15 12:21:18 +0100

    import <RecentFinds/> component

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,6 +5,7 @@ import Header from '@/components/dashboard/Header';
 import Sidebar from '@/components/dashboard/Sidebar';
 import Statistics from '@/components/dashboard/Statistics';
 import RecentXplorations from "@/components/dashboard/RecentXplorations";
+import RecentFinds from "@/components/dashboard/RecentFinds";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <section className='pt-16 px-24 w-full'>
         <Header />
         <Statistics />
+        <RecentFinds/>
         <RecentXplorations />
       </section>
     </div>
