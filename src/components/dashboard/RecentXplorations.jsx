693a4f25fbaaa79b9a96e460f22b6cd154a35785
Harman Singh <harman.singh@student.howest.be>
2023-11-15 11:41:16 +0100

    add HTML elements with styling

--- src/components/dashboard/RecentXplorations.jsx
+++ src/components/dashboard/RecentXplorations.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Title from "@/components/utility/Title";
+
 const RecentXplorations = () => {
 	const recent_xplorations = [
 		{
@@ -20,6 +22,20 @@ const RecentXplorations = () => {
 
 	return (
 		<>
+			<Title content="Recent X-Plorations" />
+
+			<div className="w-full flex flex-col gap-5 shadow">
+				<div className="h-16 flex flex-row items-center justify-between gap-5">
+					<div className="bg-black w-16 h-full">image</div>
+					<div className="w-3/6 flex items-center h-full border-black border-r-4">
+						<p className="font-bold text-lg">France</p>
+					</div>
+					<div className="w-3/6 flex flex-row justify-between items-center px-8">
+						<p className="text-brandBlack font-semibold">2 hours and 22 minutes</p>
+						<p className="font-bold text-sm">29/06/2084</p>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 };
