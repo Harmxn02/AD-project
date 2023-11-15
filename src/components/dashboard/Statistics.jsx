3bd03b93f47457b102904d3b0510cf7ddbbc13d2
layton <layton.berth@student.howest.be>
2023-11-15 10:55:29 +0100

    add area chart

--- src/components/dashboard/Statistics.jsx
+++ src/components/dashboard/Statistics.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '@/components/utility/Title'
 import BarChart from '@/components/dashboard/charts/BarChart'
+import RadarChartStacked from '@/components/dashboard/charts/RadarChartStacked'
 
 const Statistics = () => {
   return (
@@ -15,10 +16,10 @@ const Statistics = () => {
                 </div>
             </div>
             <div className='max-w-[600px]'>
-                <h3 className='text-[1.125rem] font-semibold'>Minutes flown</h3>
-                <p className='font-light'>Charting flight durations within a specific time frame, offering a clear overview for analysis.</p>
+                <h3 className='text-[1.125rem] font-semibold'>Regions</h3>
+                <p className='font-light'>A visual summary of flight time across regions, highlighting minutes flown in a specified period.</p>
                 <div className="w-full mt-4">
-                    <BarChart />
+                    <RadarChartStacked />
                 </div>
             </div>
         </div>
