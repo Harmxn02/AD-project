026221b6a4d37df3395b958ac0f2cda90c92572a
Harman Singh <harman.singh@student.howest.be>
2023-11-15 11:40:58 +0100

    add dummy data object

--- src/components/dashboard/RecentXplorations.jsx
+++ src/components/dashboard/RecentXplorations.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 const RecentXplorations = () => {
+	const recent_xplorations = [
+		{
+			countries_visited: "France",
+			duration: "2 hours and 22 minutes",
+			date: "08/08/2084",
+		},
+		{
+			countries_visited: "Egypt - United Kingdom",
+			duration: "8 hours and 44 minutes",
+			date: "14/07/2084",
+		},
+		{
+			countries_visited: "China - India - Japan",
+			duration: "26 hours and 35 minutes",
+			date: "29/06/2084",
+		},
+	];
+
 	return (
 		<>
 		</>
