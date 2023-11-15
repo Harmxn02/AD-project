f3a7852dc78dc08697f2d021c7a08b75a1dbbc77
Harman Singh <harman.singh@student.howest.be>
2023-11-15 13:46:55 +0100

    Delete alternate style of Toggle

--- src/components/utility/Toggle.jsx
+++ src/components/utility/Toggle.jsx
@@ -6,7 +6,6 @@ const Toggle = (props) => {
         <label className="relative inline-flex items-center cursor-pointer">
             <input type="checkbox" value={props.value} className="sr-only peer"/>
             <div className="w-11 h-6 bg-gray-200  peer-focus:ring-blue-300  rounded-full peer  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-brandCyan"></div>
-            {/* <div className="w-11 h-6 bg-gray-200  peer-focus:ring-blue-300               peer  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border                          after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div> */}
             <span className="ms-3 text-sm font-medium text-gray-400 peer-checked:text-black ">{props.text}</span>
         </label>
 
