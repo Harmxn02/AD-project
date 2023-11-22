1c0542870a2e3ab1a8cac400f68915e400f974df
Harman Singh <harman.singh@student.howest.be>
2023-11-22 18:01:40 +0100

    turn all custom colours into custom tailwind variables, in AccountDeleted.jsx + add new design

--- src/components/settings/AccountDeleted.jsx
+++ src/components/settings/AccountDeleted.jsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 const AccountDeleted = () => {
 	return (
-		<div className="h-screen flex flex-col justify-center items-center gap-12">
-			<p className="text-4xl font-black text-black">
+		<div className="h-screen bg-brandDarkPurple text-brandPink flex flex-col justify-center items-center gap-12">
+			<p className="text-4xl font-black text-brandPink">
 				Data succesfully deleted
 			</p>
 			<Link
 				href="/"
-				className="text-white max-w-fit font-semibold uppercase bg-[#232323] px-6 py-4"
+				className="text-white rounded-md max-w-fit font-semibold bg-brandBlue hover:bg-blue-600 px-12 py-3"
 			>
 				Return to Home Screen
 			</Link>
