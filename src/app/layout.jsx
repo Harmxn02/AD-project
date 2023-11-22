1d1c68cedadb61e07d160219850befde339cb051
Harman Singh <harman.singh@student.howest.be>
2023-11-22 14:12:07 +0100

    add custom colour and implement it

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en" className="bg-[#F6F6F6]">
+		<html lang="en" className="bg-brandGrey">
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
