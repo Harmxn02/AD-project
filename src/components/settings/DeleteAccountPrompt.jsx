f5d6142a9ab653b81c67b9e9c075c07709042834
Harman Singh <harman.singh@student.howest.be>
2023-11-22 15:15:43 +0100

    fix "next/image" import

--- src/components/settings/DeleteAccountPrompt.jsx
+++ src/components/settings/DeleteAccountPrompt.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "@/components/utility/Title";
 import Link from "next/link";
-import Image from "../../../node_modules/next/image";
+import Image from "next/image";
 
 import Danger from "../../../public/icons/section_icons/danger.svg";
 
