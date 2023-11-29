e516aee980b843b1d5222767cd0dff5747e743b5
layton <layton.berth@student.howest.be>
2023-11-29 09:49:05 +0100

    add props to chart

--- src/components/dashboard/charts/RadarChartStacked.jsx
+++ src/components/dashboard/charts/RadarChartStacked.jsx
@@ -3,53 +3,50 @@
 import React, { useEffect, useRef } from "react";
 import Chart from "chart.js/auto";
 
-const RadarChartStacked = () => {
-	const chartRef = useRef(null);
+const RadarChartStacked = (props) => {
+    const chartRef = useRef(null);
 
-	useEffect(() => {
-		const ctx = chartRef.current.getContext("2d");
+    useEffect(() => {
+        const ctx = chartRef.current.getContext("2d");
 
-		const myChart = new Chart(ctx, {
-			type: "radar",
-			data: {
-				labels: [
-					"Europe",
-					"Asia",
-					"Africa",
-					"S-America",
-					"N-America",
-					"Antarctica",
-					"Oceania",
-				],
-				datasets: [
-					{
-						data: [40, 40, 50, 10, 40, 30, 40],
-						label: "Minutes Flown",
-						backgroundColor: "#62A0AA73",
-						borderColor: "rgba(75, 192, 192, 1)",
-						borderWidth: 1,
-					},
-				],
-			},
-			options: {
-				scales: {
-					r: {
-						suggestedMin: 0,
-						suggestedMax: 60,
-					},
-				},
-				plugins: {
-					legend: false,
-				},
-			},
-		});
+        const myChart = new Chart(ctx, {
+            type: "radar",
+            data: {
+                labels: props.labels,
+                datasets: props.dataObject,
+            },
+            options: {
+                scales: {
+                    r: {
+                        suggestedMin: 0,
+                        suggestedMax: 60,
+                    },
+                },
+                plugins: {
+                    legend: false,
+                },
+            },
+        });
 
-		return () => {
-			myChart.destroy();
-		};
-	}, []);
+        return () => {
+            myChart.destroy();
+        };
+    }, []);
 
-	return <canvas ref={chartRef}></canvas>;
+    return <canvas ref={chartRef}></canvas>;
+};
+
+RadarChartStacked.propTypes = {
+    labels: PropTypes.arrayOf(PropTypes.string).isRequired,
+    dataObject: PropTypes.arrayOf(
+        PropTypes.shape({
+            label: PropTypes.string.isRequired,
+            data: PropTypes.arrayOf(PropTypes.number).isRequired,
+            backgroundColor: PropTypes.string.isRequired,
+            borderColor: PropTypes.string.isRequired,
+            borderWidth: PropTypes.number.isRequired,
+        })
+    ).isRequired,
 };
 
 export default RadarChartStacked;
