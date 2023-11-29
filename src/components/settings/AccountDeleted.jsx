e9b17d4fc1ebe76cbc861f1c7ac6deb13d98b329
Harman Singh <harman.singh@student.howest.be>
2023-11-29 10:07:31 +0100

    Change account deleted colours to match Homescreen

--- src/components/settings/AccountDeleted.jsx
+++ src/components/settings/AccountDeleted.jsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 const AccountDeleted = () => {
 	return (
-		<div className="h-screen bg-brandDarkPurple text-brandPink flex flex-col justify-center items-center gap-12">
-			<p className="text-4xl font-black text-brandPink">
+		<div className="h-screen bg-brandDarkGreen text-brandLightGreen flex flex-col justify-center items-center gap-12">
+			<p className="text-4xl font-black text-brandLightGreen">
 				Data succesfully deleted
 			</p>
 			<Link
 				href="/"
-				className="text-white rounded-md max-w-fit font-semibold bg-brandBlue hover:bg-blue-600 px-12 py-3"
+				className="text-white rounded-md max-w-fit font-semibold bg-brandTeal hover:bg-teal-800 transition-colors duration-500 px-12 py-3"
 			>
 				Return to Home Screen
 			</Link>
