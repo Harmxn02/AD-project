0391dad473d68d114e0dfe829329eb1ccb7ea86a
Harman Singh <harman.singh@student.howest.be>
2023-12-06 18:35:13 +0100

    Add Vercel Analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import PropTypes from "prop-types";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +13,10 @@ export const metadata = {
 
 const RootLayout = ({ children }) => (
 	<html lang="en" className="bg-brandBackground">
-		<body className={inter.className}>{children}</body>
+		<body className={inter.className}>
+			{children}
+			<Analytics />
+		</body>
 	</html>
 );
 
