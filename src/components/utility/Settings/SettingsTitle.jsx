d72252d9838ecaff8adb384e04c50b6755ef98c6
Harman Singh <harman.singh@student.howest.be>
2023-12-07 23:53:43 +0100

    add prop-validation to SettingsTitle.jsx

--- src/components/utility/Settings/SettingsTitle.jsx
+++ src/components/utility/Settings/SettingsTitle.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
+import PropTypes from "prop-types";
+
 const SettingsTitle = (props) => {
+	SettingsTitle.propTypes = {
+		children: PropTypes.node.isRequired,
+	};
 	return <h3 className="font-medium text-2xl">{props.children}</h3>;
 };
 
