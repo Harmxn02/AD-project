37a2efae57062307f0ee824b4377c4f88f1ec2a4
Layton Berth <laytonbrth@gmail.com>
2023-12-08 16:24:59 +0100

    add structure refactoring

--- src/components/utility/Layout.jsx
+++ src/components/utility/Layout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import Sidebar from "@/components/dashboard/Sidebar";
 
-const Layout = ({ children }) => (
-  <div className="flex">
-    <Sidebar />
-    {children}
-  </div>
-);
+const Layout = ({ children }) => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      {children}
+    </div>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
