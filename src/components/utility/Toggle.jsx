b78825c7850fde85da61668cce94946c7cf41ef1
layton <layton.berth@student.howest.be>
2023-12-08 23:29:50 +0100

    add state checked functionality in toggle

--- src/components/utility/Toggle.jsx
+++ src/components/utility/Toggle.jsx
@@ -8,6 +8,7 @@ const Toggle = (props) => {
 				<input
 					type="checkbox"
 					value={props.value}
+					checked={props.state}
 					className="sr-only peer"
 				/>
 				<div className="w-11 h-6 bg-gray-200  peer-focus:ring-blue-300  rounded-full peer  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-brandTeal"></div>
@@ -22,6 +23,7 @@ const Toggle = (props) => {
 Toggle.propTypes = {
 	value: PropTypes.string,
 	text: PropTypes.string,
+	state: PropTypes.bool
 };
 
 export default Toggle;
