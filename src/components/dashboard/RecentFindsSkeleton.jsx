f70d42e2564132bc36766493f6848e9feb14f4df
Harman Singh <harman.singh@student.howest.be>
2023-12-09 14:43:01 +0100

    add animate pulse

--- src/components/dashboard/RecentFindsSkeleton.jsx
+++ src/components/dashboard/RecentFindsSkeleton.jsx
@@ -19,35 +19,35 @@ const RecentFindsSkeleton = () => {
 
 			<div className="w-full flex flex-col gap-5">
 				<RecentsContainer>
-					<div className="bg-gray-200 w-16 h-16 flex rounded-xl rounded-tr-none rounded-br-none justify-center text-white items-center"></div>
+					<div className="bg-gray-200 animate-pulse w-16 h-16 flex rounded-xl rounded-tr-none rounded-br-none justify-center text-white items-center"></div>
 					<div className="w-3/6 flex items-center h-full border-r-4">
-						<div className="h-[1rem] w-[30ch] rounded-full bg-gray-200"></div>
+						<div className="h-[1rem] w-[30ch] rounded-full bg-gray-200 animate-pulse"></div>
 					</div>
 					<div className="w-3/6 flex flex-row justify-between items-center px-8">
-						<div className="h-[1rem] w-[8ch] rounded-full bg-gray-200"></div>
-						<div className="h-[1rem] w-[8ch] rounded-full bg-gray-200"></div>
+						<div className="h-[1rem] w-[8ch] rounded-full bg-gray-200 animate-pulse"></div>
+						<div className="h-[1rem] w-[8ch] rounded-full bg-gray-200 animate-pulse"></div>
 					</div>
 				</RecentsContainer>
 
                 <RecentsContainer>
-					<div className="bg-gray-200 w-16 h-16 flex rounded-xl rounded-tr-none rounded-br-none justify-center text-white items-center"></div>
+					<div className="bg-gray-200 animate-pulse w-16 h-16 flex rounded-xl rounded-tr-none rounded-br-none justify-center text-white items-center"></div>
 					<div className="w-3/6 flex items-center h-full border-r-4">
-						<div className="h-[1rem] w-[30ch] rounded-full bg-gray-200"></div>
+						<div className="h-[1rem] w-[30ch] rounded-full bg-gray-200 animate-pulse"></div>
 					</div>
 					<div className="w-3/6 flex flex-row justify-between items-center px-8">
-						<div className="h-[1rem] w-[8ch] rounded-full bg-gray-200"></div>
-						<div className="h-[1rem] w-[8ch] rounded-full bg-gray-200"></div>
+						<div className="h-[1rem] w-[8ch] rounded-full bg-gray-200 animate-pulse"></div>
+						<div className="h-[1rem] w-[8ch] rounded-full bg-gray-200 animate-pulse"></div>
 					</div>
 				</RecentsContainer>
 
                 <RecentsContainer>
-					<div className="bg-gray-200 w-16 h-16 flex rounded-xl rounded-tr-none rounded-br-none justify-center text-white items-center"></div>
+					<div className="bg-gray-200 animate-pulse w-16 h-16 flex rounded-xl rounded-tr-none rounded-br-none justify-center text-white items-center"></div>
 					<div className="w-3/6 flex items-center h-full border-r-4">
-						<div className="h-[1rem] w-[30ch] rounded-full bg-gray-200"></div>
+						<div className="h-[1rem] w-[30ch] rounded-full bg-gray-200 animate-pulse"></div>
 					</div>
 					<div className="w-3/6 flex flex-row justify-between items-center px-8">
-						<div className="h-[1rem] w-[8ch] rounded-full bg-gray-200"></div>
-						<div className="h-[1rem] w-[8ch] rounded-full bg-gray-200"></div>
+						<div className="h-[1rem] w-[8ch] rounded-full bg-gray-200 animate-pulse"></div>
+						<div className="h-[1rem] w-[8ch] rounded-full bg-gray-200 animate-pulse"></div>
 					</div>
 				</RecentsContainer>
 
