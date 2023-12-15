f316d09240a6c6aa776ab70f6bea5e9b9d0615e0
Harman Singh <harman.singh@student.howest.be>
2023-12-15 01:54:12 +0100

    undo changes made for easier development

--- src/components/settings/subscription/SubscriptionsPage.jsx
+++ src/components/settings/subscription/SubscriptionsPage.jsx
@@ -8,40 +8,38 @@ import VerticalSubscriptionCard from "./VerticalSubscriptionCard";
 
 import SubscriptionsPageSkeleton from "@/components/utility/skeletons/SubscriptionsPageSkeleton";
 
-
 import { GetAPI } from "../../../assets/js/api";
 
 const SubscriptionsPage = () => {
 	// normally `const endpoint`, but I need to splice the first 3 elements
 	let plans = GetAPI("/plans");
 
-	if (plans) {
-		// this prevents map from not working if the data is empty
+	if (!plans) {
 		return <SubscriptionsPageSkeleton />;
 	}
 
-	// plans = plans.slice(0, 3);
+	plans = plans.slice(0, 3);
 
-	// return (
-	// 	<section className="mb-12 ">
-	// 		<Title content="Subscription Plans" />
-	// 		<div className="flex justify-between">
-	// 			{/* {plans.map((subscription) => (
-	// 				<SubscriptionCard
-	// 					isLast={subscription.id === plans.length}
-	// 					key={subscription.id}
-	// 					price={subscription.price}
-	// 					name={subscription.name}
-	// 				>
-	// 					{subscription.perks.map((perk) => (
-	// 						<ListItem key={perk} content={perk} />
-	// 					))}
-	// 				</SubscriptionCard>
-	// 			))} */}
-	// 		</div>
-	// 		<VerticalSubscriptionCard />
-	// 	</section>
-	// );
+	return (
+		<section className="mb-12 ">
+			<Title content="Subscription Plans" />
+			<div className="flex justify-between">
+				{plans.map((subscription) => (
+					<SubscriptionCard
+						isLast={subscription.id === plans.length}
+						key={subscription.id}
+						price={subscription.price}
+						name={subscription.name}
+					>
+						{subscription.perks.map((perk) => (
+							<ListItem key={perk} content={perk} />
+						))}
+					</SubscriptionCard>
+				))}
+			</div>
+			<VerticalSubscriptionCard />
+		</section>
+	);
 };
 
 export default SubscriptionsPage;
