1c10ee49c909bb3f518573f5b1a3e4a4227472ba
Harman Singh <harman.singh@student.howest.be>
2023-12-17 16:24:35 +0100

    remove unused custom colours + format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,44 +1,38 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        brandCyan: "#62A0AA",
-        brandTeal: "#54696c",
-        brandBlack: "#4C4C4C",
-        brandGrey: "#B3B3B3",
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				brandCyan: "#62A0AA",
+				brandTeal: "#54696c",
+				brandBlack: "#4C4C4C",
 
-        // used for background of entire client
-        brandBackground: "#FAFAFA",
+				// used for background of entire client
+				brandBackground: "#FAFAFA",
 
-        // used for Recent Finds
-        brandRed: "#E74C3C",
-        brandGreen: "#2ECC71",
-        brandYellow: "#F1C40F",
+				// used for Recent Finds
+				brandRed: "#E74C3C",
+				brandGreen: "#2ECC71",
+				brandYellow: "#F1C40F",
 
-        // previously used for Homescreen
-        brandDarkPurple: "#00002e",
-        brandPink: "#D292FF",
-        // used for Homescreen
-        brandDarkGreen: "#002e2c",
-        brandLightGreen: "#92ffd0",
-        brandBlue: "#3C46FF",
-        brandLightGrey: "#8E8EA0",
-        // used for specific areas
-        lastPlan: "#3E3E3E",
-        alternatingTransaction: "#DFE5EB",
-        planButtons: "#2C2C2C",
-      },
-    },
-  },
-  plugins: [
-    require('tailwind-scrollbar'),
-  ],
-}
-export default config
+				// used for Homescreen
+				brandDarkGreen: "#002e2c",
+				brandLightGreen: "#92ffd0",
+				brandLightGrey: "#8E8EA0",
+
+				// used for specific areas
+				lastPlan: "#3E3E3E",
+				alternatingTransaction: "#DFE5EB",
+				planButtons: "#2C2C2C",
+			},
+		},
+	},
+	plugins: [require("tailwind-scrollbar")],
+};
+export default config;
