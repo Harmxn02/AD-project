69d084f1aa7e977dd594acc3c237f819e09853cc
Harman Singh <harman.singh@student.howest.be>
2023-12-18 15:41:07 +0100

    saving to localeStorage works

--- src/components/homescreen/ProfileSelector.jsx
+++ src/components/homescreen/ProfileSelector.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import useLocalStorage from "../../assets/js/useLocalStorage";
 
 import MaleOne from "../../../public/profile-pictures/male_1.png";
 import MaleTwo from "../../../public/profile-pictures/male_2.jpg";
@@ -26,6 +27,15 @@ const people = [
 ];
 
 const ProfileSelector = () => {
+	const [selectedUser, setSelectedUser] = useLocalStorage("selectedUser", "");
+
+	const handleProfileChange = (event) => {
+		const selectedId = parseInt(event.target.value, 10);
+		const selectedUser = people.find((person) => person.id === selectedId);
+
+		setSelectedUser(JSON.stringify(selectedUser)); // Save the selected user to localStorage
+	};
+
 	return (
 		<div className="absolute bottom-40">
 			<p className="text-sm font-medium text-gray-900 my-2 text-center">
@@ -33,6 +43,8 @@ const ProfileSelector = () => {
 			</p>
 			<select
 				className="px-4 py-2 border rounded-md"
+				value={selectedUser ? JSON.parse(selectedUser).id : ""}
+				onChange={handleProfileChange}
 			>
 				{people.map((person) => (
 					<option key={person.id} value={person.id}>
