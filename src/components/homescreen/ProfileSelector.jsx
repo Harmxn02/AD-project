9eba39ffd2a0446a1b7fe4e6b9f474b3bafe9946
Harman Singh <harman.singh@student.howest.be>
2023-12-18 12:17:46 +0100

    working dropdown without images

--- src/components/homescreen/ProfileSelector.jsx
+++ src/components/homescreen/ProfileSelector.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 import MaleOne from "../../../public/profile-pictures/male_1.png";
 import MaleTwo from "../../../public/profile-pictures/male_2.jpg";
 import FemaleOne from "../../../public/profile-pictures/female_1.png";
 
-const users = [
-	{ id: 1, name: "Male 1", src: MaleOne, alt: "Male 1" },
-	{ id: 2, name: "Male 2", src: MaleTwo, alt: "Male 2" },
-	{ id: 3, name: "Female 1", src: FemaleOne, alt: "Female 1" },
+const people = [
+	{
+		id: 1,
+		src: MaleOne,
+		alt: "MaleOne",
+		name: "Wade Cooper",
+		avatar: "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+	},
+	{
+		id: 2,
+		src: MaleTwo,
+		alt: "MaleTwo",
+		name: "Devon Webb",
+		avatar: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80",
+	},
+	{
+		id: 3,
+		src: FemaleOne,
+		alt: "FemaleOne",
+		name: "Arlene Mccoy",
+		avatar: "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+	},
 ];
 
 const ProfileSelector = () => {
@@ -22,39 +40,30 @@ const ProfileSelector = () => {
 		}
 	}, [selectedElement]);
 
-	const commonStyles = "rounded-full border-2";
-	const selectedStyles =
-		"h-24 w-24  border-4 border-blue-500";
-
-	const handleClick = (adriaID) => {
-		// Toggle selection
-		const newSelectedElement = selectedElement === adriaID ? null : adriaID;
-		setSelectedElement(newSelectedElement);
-
-		// Update local storage
-		localStorage.setItem("AdriaID", newSelectedElement);
-	};
+	const handleChange = (event) => {
+        const newSelectedElement = parseInt(event.target.value, 10);
+        setSelectedElement(newSelectedElement);
+    
+        // Update local storage
+        localStorage.setItem("AdriaID", newSelectedElement);
+      };
 
 	return (
 		<div className="absolute top-40">
-			<p className="text-xl font-semibold text-brandBlack my-2 text-center">
-				Select user:
+			<p className="text-sm font-medium text-gray-900 my-2 text-center">
+				Continue as:
 			</p>
-			<div className="flex gap-4 items-center">
-				{users.map((user) => (
-					<Image
-						key={user.id}
-						src={user.src}
-						alt={user.alt}
-						className={`${commonStyles} ${
-							selectedElement === user.id
-								? selectedStyles
-								: "h-16 w-16"
-						}`}
-						onClick={() => handleClick(user.id)}
-					/>
+			<select
+				value={selectedElement}
+				onChange={handleChange}
+				className="px-4 py-2 border rounded-md"
+			>
+				{people.map((person) => (
+					<option key={person.id} value={person.id}>
+						{person.name}
+					</option>
 				))}
-			</div>
+			</select>
 		</div>
 	);
 };
