96e9d11cb15293cd619e0da9d66646f7eb8589e7
Harman Singh <harman.singh@student.howest.be>
2023-12-18 15:05:11 +0100

    remove saving mechanim, just so build works

--- src/components/homescreen/ProfileSelector.jsx
+++ src/components/homescreen/ProfileSelector.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useState, useEffect } from "react";
 
 import MaleOne from "../../../public/profile-pictures/male_1.png";
@@ -28,40 +26,12 @@ const people = [
 ];
 
 const ProfileSelector = () => {
-	const [selectedUser, setSelectedUser] = useState(null);
-
-	useEffect(() => {
-		// Set DEFAULT value to the second user if selectedUser is initially null
-		if (selectedUser === null) {
-			const defaultUser = people.find((person) => person.id === 2);
-			setSelectedUser(defaultUser);
-
-			// Check if running on the client side before using localStorage
-			if (typeof window !== "undefined") {
-				localStorage.setItem("AdriaUser", JSON.stringify(defaultUser));
-			}
-		}
-	}, [selectedUser]);
-
-	const handleChange = (event) => {
-		const newSelectedId = parseInt(event.target.value, 10);
-		const newUser = people.find((person) => person.id === newSelectedId);
-		setSelectedUser(newUser);
-
-		// Check if running on the client side before using localStorage
-		if (typeof window !== "undefined") {
-			localStorage.setItem("AdriaUser", JSON.stringify(newUser));
-		}
-	};
-
 	return (
 		<div className="absolute bottom-40">
 			<p className="text-sm font-medium text-gray-900 my-2 text-center">
 				Continue as:
 			</p>
 			<select
-				value={selectedUser ? selectedUser.id : ""}
-				onChange={handleChange}
 				className="px-4 py-2 border rounded-md"
 			>
 				{people.map((person) => (
