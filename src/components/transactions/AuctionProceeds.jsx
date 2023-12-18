0fb0636e6d38e6247d17982de2c4467d62897fbc
Harman Singh <harman.singh@student.howest.be>
2023-12-18 14:14:46 +0100

    potential fix: use Laytons method for accessing localStorage

--- src/components/transactions/AuctionProceeds.jsx
+++ src/components/transactions/AuctionProceeds.jsx
@@ -10,7 +10,13 @@ import Skeleton from "@/components/utility/skeletons/TransactionsSkeleton";
 import { GetAPI } from "../../assets/js/api";
 
 const AuctionProceeds = () => {
-	const adriaId = JSON.parse(localStorage.getItem("AdriaUser")).id;
+	let adriaId;
+
+	if (typeof window !== "undefined") {
+		if (localStorage.getItem("AdriaUser")) {
+			adriaId = JSON.parse(localStorage.getItem("AdriaUser")).id;
+		}
+	}
 	const auction_proceeds = GetAPI(`/auctions/${adriaId}`);
 
 	if (auction_proceeds === null) {
