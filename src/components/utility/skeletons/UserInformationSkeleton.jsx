1e2eac2dc3a4061870e0e52ce4abedab120a5ddc
Harman Singh <harman.singh@student.howest.be>
2023-12-19 09:41:31 +0100

    add border-radius to skeleton loader div

--- src/components/utility/skeletons/UserInformationSkeleton.jsx
+++ src/components/utility/skeletons/UserInformationSkeleton.jsx
@@ -15,8 +15,8 @@ const UserInformationSkeleton = () => {
 					<path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
 				</svg>
 			</div>
-			<div className="h-4 w-[13ch] mx-auto bg-brandTeal animate-pulse mt-2 "></div>
-			<div className="h-3 w-[15ch] mx-auto bg-gray-200 animate-pulse"></div>
+			<div className="h-4 w-[13ch] mx-auto bg-brandTeal animate-pulse rounded-full mt-4 "></div>
+			<div className="h-3 w-[15ch] mx-auto bg-gray-200 animate-pulse rounded-full"></div>
 		</div>
 	);
 };
