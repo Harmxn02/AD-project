31d848ba78dbe31197614c907b2e6b29ee5cb7dc
layton <layton.berth@student.howest.be>
2023-12-20 15:29:40 +0100

    change red color to blue ish color for the sea

--- src/components/modals/RecentFindsModal.jsx
+++ src/components/modals/RecentFindsModal.jsx
@@ -4,7 +4,7 @@ const RecentFindsModal = ({ onClose }) => {
 	return (
 		<div className="fixed top-0 left-0 flex w-full h-full justify-center items-center bg-black bg-opacity-50">
 			<div className="bg-white w-[90%] h-[85%] flex">
-				<div className="bg-red-500 w-full min-w-[80%]">
+				<div className="bg-[#85cbfa] w-full min-w-[80%]">
 					<Map />
 				</div>
 				<div className="bg-gray-500 w-full">
