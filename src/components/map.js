d1bad8d6edae083f6ef3dac54b2bb91e521c7dfe
layton <layton.berth@student.howest.be>
2023-12-20 16:57:03 +0100

    add normal fetch and no getApi function due hook errors.

--- src/components/map.js
+++ src/components/map.js
@@ -1,36 +1,42 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, {
+    useRef,
+    useEffect
+} from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 export default function Map() {
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng] = useState(139.753);
-    const [lat] = useState(35.6844);
-    const [zoom] = useState(7);
-    const [API_KEY] = useState('OZkqnFxcrUbHDpJQ5a3K');
+    const lng = 139.753;
+    const lat = 35.6844;
+    const zoom = 7;
+    const API_KEY = "OZkqnFxcrUbHDpJQ5a3K";
 
-  useEffect(() => {
-    if (map.current) return; // stops map from intializing more than once
+    useEffect(() => {
+        if (map.current) return; // stops map from intializing more than once
 
-    map.current = new maplibregl.Map({
-      container: mapContainer.current,
-      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
-      center: [lng, lat],
-      zoom: zoom,
-      minZoom: 1,
-      maxZoom: 16,
-    });
+        map.current = new maplibregl.Map({
+            container: mapContainer.current,
+            style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
+            center: [lng, lat],
+            zoom: zoom,
+            minZoom: 1,
+            maxZoom: 16,
+        });
 
-    new maplibregl.Marker({color: "#FF0000"})
-      .setLngLat([lng,lat])
-      .addTo(map.current);
+        renderMarkers();
 
-  }, [API_KEY, lng, lat, zoom]);
+    }, [API_KEY, lng, lat, zoom]);
 
-  return (
-    <div className="map-wrap relative w-full h-full">
-      <div ref={mapContainer} className="map absolute w-full h-full"/>
-    </div>
-  );
+    async function renderMarkers() {
+        const { sessions } = await fetch("https://project-2.ti.howest.be/2023-2024/group-17/api/sessions/1").then(data => data.json());
+        console.log(sessions)
+    }
+
+    return (
+        <div className="map-wrap relative w-full h-full">
+            <div ref={mapContainer} className="map absolute w-full h-full" />
+        </div>
+    );
 }
