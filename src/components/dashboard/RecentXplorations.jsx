2fdf0078b71da632523d9bd50e058d961d0828e6
Layton Berth <laytonbrth@gmail.com>
2023-12-21 00:55:00 +0100

    remove double country names

--- src/components/dashboard/RecentXplorations.jsx
+++ src/components/dashboard/RecentXplorations.jsx
@@ -149,9 +149,6 @@ const RecentXplorations = () => {
 								</span>
 							</button>
 						</div>
-						<div className="w-3/6 flex items-center h-full border-r-4">
-							<p className="font-medium text-lg">{exploration.countriesVisited.join(" - ")}</p>
-						</div>
 						<div className="w-3/6 flex flex-row justify-between items-center px-8">
 							<p className="text-brandBlack font-semibold">
 								{formatTimeDifference(exploration.startTime, exploration.endTime)}
