940c16ce838a912210305a7be0eeafd0cebd3c92
layton <layton.berth@student.howest.be>
2023-12-20 17:59:53 +0100

    change component name

--- src/components/modals/RecentFindsModal.jsx
+++ src/components/modals/RecentFindsModal.jsx
@@ -1,11 +1,11 @@
-import Map from '@/components/map';
+import RouteMap from '@/components/RouteMap';
 
 const RecentFindsModal = ({ onClose }) => {
 	return (
 		<div className="fixed top-0 left-0 flex w-full h-full justify-center items-center bg-black bg-opacity-50">
 			<div className="bg-white w-[90%] h-[85%] flex">
 				<div className="bg-[#85cbfa] w-full min-w-[80%]">
-					<Map />
+					<RouteMap />
 				</div>
 				<div className="bg-gray-500 w-full">
 				</div>
