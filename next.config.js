2cfba515bcca4ecbbe28ea8b2ac42da345c21e4c
layton <layton.berth@student.howest.be>
2023-12-22 00:44:39 +0100

    Update remotePatterns in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: ["images.unsplash.com"],
+		remotePatterns: [{
+			protocol: 'https',
+			hostname: 'images.unsplash.com'
+		  }
+		],
 	},
 };
 
