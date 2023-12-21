9c4675b7bf5c54c5e6875fd2d88ae928a9631264
layton <layton.berth@student.howest.be>
2023-12-21 19:20:16 +0100

    refactor code and made it working

--- src/components/settings/subscription/SubscriptionsPage.jsx
+++ src/components/settings/subscription/SubscriptionsPage.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Title from "@/components/utility/Title";
 import SubscriptionCard from "./SubscriptionCard";
 import ListItem from "./ListItem";
@@ -10,71 +10,93 @@ import SubscriptionsPageSkeleton from "@/components/utility/skeletons/Subscripti
 
 import { GetAPI } from "../../../assets/js/api";
 
+const fetchPlanId = async (adriaId) => {
+  try {
+    const response = await fetch(
+      `https://project-2.ti.howest.be/2023-2024/group-17/api/members/${adriaId}`
+    );
+
+    if (response.ok) {
+      const { member } = await response.json();
+      return member.subscription.plan.id;
+    } else {
+      console.error("Failed to fetch plan");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching plan data:", error);
+    return null;
+  }
+};
+
+const switchPlan = async (adriaId, subscriptionId) => {
+  try {
+    const response = await fetch(
+      `https://project-2.ti.howest.be/2023-2024/group-17/api/members/${adriaId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ subscriptionId }),
+      }
+    );
+
+    if (!response.ok) {
+      console.error("Failed to switch plan");
+    }
+  } catch (error) {
+    console.error("Error while fetching the PUT endpoint", error);
+  }
+};
+
 const SubscriptionsPage = () => {
-	const adriaId = 1;
-	const memberData = GetAPI(`/members/${adriaId}`);
-	const subscriptionIdOfMember = memberData
-		? memberData.subscriptionId
-		: null;
-	const plan = GetAPI(`/subscribe/${subscriptionIdOfMember}`);
-	const planId = plan ? plan.id : null;
+  const adriaId = 1;
+  const [planId, setPlanId] = useState(null);
 
-	const handleSwitchPlan = async (subscriptionId) => {
-		try {
-			const response = await fetch(
-				`https://project-2.ti.howest.be/2023-2024/group-17/api/members/${adriaId}`,
-				{
-					method: "PUT",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({ subscriptionId }),
-				},
-			);
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedPlanId = await fetchPlanId(adriaId);
+      setPlanId(fetchedPlanId);
+    };
 
-			if (response.ok) {
-				console.log("Plan switched successfully");
-			} else {
-				console.error("Failed to switch plan");
-			}
-		} catch (error) {
-			console.error("Error while switching plan", error);
-		}
-	};
+    fetchData();
+  }, [adriaId]);
 
-	// normally `const endpoint`, but I need to splice the first 3 elements
-	let plans = GetAPI("/plans");
+  const handleSwitchPlan = (subscriptionId) => {
+    switchPlan(adriaId, subscriptionId);
+  };
 
-	if (!plans) {
-		return <SubscriptionsPageSkeleton />;
-	}
+  let plans = GetAPI("/plans");
 
-	plans = plans.slice(0, 3);
+  if (!plans) {
+    return <SubscriptionsPageSkeleton />;
+  }
 
-	return (
-		<section className="mb-12 ">
-			<Title content="Subscription Plans" />
-			<div className="flex justify-between">
-				{plans.map((subscription) => (
-					<SubscriptionCard
-						isLast={subscription.id === plans.length}
-						isActive={subscription.id === planId} // Compare subscription.id with planId
-						onActivate={() => {
-							handleSwitchPlan(subscription.id);
-						}}
-						key={subscription.id}
-						price={subscription.price}
-						name={subscription.name}
-					>
-						{subscription.perks.map((perk) => (
-							<ListItem key={perk} content={perk} />
-						))}
-					</SubscriptionCard>
-				))}
-			</div>
-			<VerticalSubscriptionCard />
-		</section>
-	);
+  plans = plans.slice(0, 3);
+
+  return (
+    <section className="mb-12 ">
+      <Title content="Subscription Plans" />
+      <div className="flex justify-between">
+        {plans.map((subscription) => (
+          <SubscriptionCard
+            key={subscription.id}
+            isLast={subscription.id === plans.length}
+            isActive={subscription.id === planId}
+            onActivate={() => handleSwitchPlan(subscription.id)}
+            price={subscription.price}
+            name={subscription.name}
+          >
+            {subscription.perks.map((perk) => (
+              <ListItem key={perk} content={perk} />
+            ))}
+          </SubscriptionCard>
+        ))}
+      </div>
+      <VerticalSubscriptionCard />
+    </section>
+  );
 };
 
 export default SubscriptionsPage;
