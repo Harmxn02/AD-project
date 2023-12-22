3eddde22f8fd2b602dc1dfe7e8486d484fc99922
layton <layton.berth@student.howest.be>
2023-12-22 20:53:31 +0100

    remove settings on deletepage change

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -27,7 +27,7 @@ const SidebarLink = ({ href, icon, text, currentPath }) => {
 		currentPath: PropTypes.string.isRequired,
 	};
 
-	const isActive = currentPath.includes(href) || currentPath === href;;
+	const isActive = currentPath === href;;
 
 	return (
 		<li className="flex items-center">
