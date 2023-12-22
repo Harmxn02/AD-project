88f31a1458355fcb0fe84d92f3522f00d784db93
Harman Singh <harman.singh@student.howest.be>
2023-12-22 14:20:00 +0100

    codesmell: define constant instead of duplicating

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -170,12 +170,12 @@ const Sidebar = () => {
 					width="24"
 					height="24"
 					viewBox="0 0 24 24"
-					fill={pathname.startsWith("/transactions") ? "#54696c" : "#4C4C4C"}
+					fill={pathname.startsWith(transactionsPathname) ? "#54696c" : "#4C4C4C"}
 					xmlns="http://www.w3.org/2000/svg"
 				>
 					<path
 						d="M2 7H20M16 2L21 7L16 12M22 17H4M8 12L3 17L8 22"
-						stroke={pathname.startsWith("/transactions") ? "#54696c" : "#4C4C4C"}
+						stroke={pathname.startsWith(transactionsPathname) ? "#54696c" : "#4C4C4C"}
 						strokeWidth="2"
 					/>
 				</svg>
