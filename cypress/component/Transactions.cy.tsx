4681b58cadb4bb1089c18ea19f8c96a3928a2142
layton <layton.berth@student.howest.be>
2023-12-22 20:26:38 +0100

    add transactions test

--- cypress/component/Transactions.cy.tsx
+++ cypress/component/Transactions.cy.tsx
@@ -1,20 +1,20 @@
-import AuctionProceeds from "@/components/transactions/AuctionProceeds";
+import Transactions from "@/components/transactions/Transactions";
 
-describe("AuctionProceeds component", () => {
+describe("Transactions component", () => {
 	beforeEach(() => {
-		cy.mount(<AuctionProceeds />);
+		cy.mount(<Transactions />);
 	});
 
 	it('renders loading skeleton when data is null', () => {
         // Check if the component renders with loading state (skeleton)
         cy.get('.text-black.font-bold.text-3xl.my-4').should('exist');
-        cy.contains('An overview of your earnings from the auction.').should('exist');
+        cy.contains('An overview of your purchases.').should('exist');
       });
 
     it('renders auction proceeds table with data', () => {
         // Check if the component renders with the correct title and description
         cy.get('section > h2.text-black.font-bold.text-3xl.my-4').should('exist');
-        cy.contains('An overview of your earnings from the auction.').should('exist');
+        cy.contains('An overview of your purchases.').should('exist');
 
         // Check if the table is rendered with the correct headers
         cy.get('thead tr').should('have.length', 1);
